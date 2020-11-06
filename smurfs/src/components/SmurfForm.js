aed584475689f5e97f57d2cf5da878786354bf34
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-06 10:33:52 -0600

    dependencies installed

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const SmurfForm = () => {
+    
+    return (
+        <div className="smurfForm">
+            <h2>Add a Smurf!</h2>
+        </div>
+    )
+}
+
+export default SmurfForm;
